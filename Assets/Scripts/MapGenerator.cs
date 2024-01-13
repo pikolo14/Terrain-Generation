20a9ff975b604c3480103ab99aa237c7efbe7b3a
jpicogom <jmpicgom@yahoo.es>
2024-01-13 09:40:48 +0100

    Generacion de una textura de noise escalable y redimensionable

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+	public int mapWidth;
+	public int mapHeight;
+	public float noiseScale;
+
+	public bool autoUpdate;
+
+	/// <summary>
+	/// Funcion principal que genera el mapa y su visualizacion
+	/// </summary>
+	public void GenerateMap() {
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
+
+		NoiseMapView display = FindObjectOfType<NoiseMapView> ();
+		display.DrawNoiseMap (noiseMap);
+	}
+	
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Noise {
+
+	/// <summary>
+	/// Devuelve una matriz de valores entre 0 a 1 siguiendo la función
+	/// </summary>
+	/// <param name="mapWidth"></param>
+	/// <param name="mapHeight"></param>
+	/// <param name="scale"> Escala el tamaño del ruido. A menor escala, mas se parecerá a la estática de la television</param>
+	/// <returns></returns>
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
+		float[,] noiseMap = new float[mapWidth,mapHeight];
+
+		if (scale <= 0) {
+			scale = 0.0001f;
+		}
+
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float sampleX = x / scale;
+				float sampleY = y / scale;
+
+				float perlinValue = Mathf.PerlinNoise (sampleX, sampleY);
+				noiseMap [x, y] = perlinValue;
+			}
+		}
+
+		return noiseMap;
+	}
+
+}
