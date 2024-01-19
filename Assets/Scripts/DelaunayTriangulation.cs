dc58b10795bc4ab2bee584d8d3eb79340f10bb87
jpicogom <jmpicgom@yahoo.es>
2024-01-19 17:34:09 +0100

    Mesh con triangulacion delaunay sobre nodepoints

---
+++ Assets/Scripts/DelaunayTriangulation.cs
@@ -0,0 +1,72 @@
+using Habrador_Computational_Geometry;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class DelaunayTriangulation
+{
+    public static Mesh GetDelaunayTriangleMesh(List<Vector2> points)
+    {
+        //Hull
+        List<MyVector2> hullPoints_2d = points.Select(point => point.ToMyVector2()).ToList();
+
+        //Normalize to range 0-1
+        //We should use all points, including the constraints because the hole may be outside of the random points
+        List<MyVector2> allPoints = new List<MyVector2>();
+
+        //allPoints.AddRange(randomPoints_2d);
+
+        allPoints.AddRange(hullPoints_2d);
+
+        Normalizer2 normalizer = new Normalizer2(allPoints);
+
+        List<MyVector2> hullPoints_2d_normalized = normalizer.Normalize(hullPoints_2d);
+
+        HashSet<List<MyVector2>> allHolePoints_2d_normalized = new HashSet<List<MyVector2>>();
+
+
+
+        // Generate the triangulation
+
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+
+        timer.Start();
+
+        //Algorithm 1. Delaunay by triangulate all points with some bad algorithm and then flip edges until we get a delaunay triangulation 
+        //HalfEdgeData2 triangleData_normalized = _Delaunay.FlippingEdges(points_2d_normalized, new HalfEdgeData2());
+
+
+        //Algorithm 2. Delaunay by inserting point-by-point while flipping edges after inserting a single point 
+        HalfEdgeData2 triangleData_normalized = _Delaunay.PointByPoint(hullPoints_2d_normalized.ToHashSet(), new HalfEdgeData2());
+
+        ////Algorithm 3. Constrained delaunay
+        //HalfEdgeData2 triangleData_normalized = _Delaunay.ConstrainedBySloan(null, hullPoints_2d_normalized, allHolePoints_2d_normalized, shouldRemoveTriangles: true, new HalfEdgeData2());
+
+        timer.Stop();
+
+        Debug.Log($"Generated a delaunay triangulation in {timer.ElapsedMilliseconds / 1000f} seconds");
+
+
+        //UnNormalize
+        HalfEdgeData2 triangleData = normalizer.UnNormalize(triangleData_normalized);
+
+        //From half-edge to triangle
+        HashSet<Triangle2> triangles_2d = _TransformBetweenDataStructures.HalfEdge2ToTriangle2(triangleData);
+
+        //From triangulation to mesh
+
+        //Make sure the triangles have the correct orientation
+        triangles_2d = HelpMethods.OrientTrianglesClockwise(triangles_2d);
+
+        //From 2d to 3d
+        HashSet<Triangle3> triangles_3d = new HashSet<Triangle3>();
+
+        foreach (Triangle2 t in triangles_2d)
+        {
+            triangles_3d.Add(new Triangle3(t.p1.ToMyVector3_Yis3D(), t.p2.ToMyVector3_Yis3D(), t.p3.ToMyVector3_Yis3D()));
+        }
+
+        return _TransformBetweenDataStructures.Triangle3ToCompressedMesh(triangles_3d);
+    }
+}
--- Assets/Scripts/NodePointsGenerator.cs
+++ Assets/Scripts/NodePointsGenerator.cs
@@ -1,5 +1,8 @@
+using Habrador_Computational_Geometry;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -10,10 +13,12 @@ public class NodePointsGenerator : MonoBehaviour
     private MapView _mapView;
     public GameObject NodePointPrefab;
     public float HeightOffset = 1;
-    public GameObject[] Nodes;
+    public List<GameObject> Nodes;
 
     public bool AutoResetSeed = true;
     private int _currentSeed;
+
+    public MeshFilter DebugTrianglesMesh;
 
 
     [ExecuteAlways]
@@ -26,31 +31,45 @@ public class NodePointsGenerator : MonoBehaviour
 
         Vector2 zoneSize = new Vector2(_mapGenerator.MapWidth, _mapGenerator.MapHeight);
         List<Vector2> nodePoints = PointsGeneration.GeneratePoissonDiscPoints(DistanceRadius, _mapView.transform.position, zoneSize, _currentSeed);
-        Nodes = new GameObject[nodePoints.Count];
+        Nodes = new List<GameObject>();
 
-        //float height = HeightOffset + _mapGenerator.transform.position.y;
+        float height = HeightOffset + _mapGenerator.transform.position.y;
 
+        ////Colocamos los puntos a una altura fija formando un plano sobre el terreno
         //for (int i = 0; i < nodePoints.Count; i++)
         //{
         //    Vector2 point = nodePoints[i];
         //    GameObject nodeGO = Instantiate(NodePointPrefab, transform);
         //    nodeGO.transform.position = new Vector3(point.x, height, point.y);
-        //    Nodes[i] = nodeGO;
+        //    Nodes.Add(nodeGO);
         //}
 
-        for(int i=0; i < nodePoints.Count; i++)
+        //Hacemos un Raycast sobre el terreno para obtener la ubicacion de los nodos pegados a la tierra
+        //TODO: Mejorar método para que los puntos en los límites colisionen con en el terreno
+        for (int i = 0; i < nodePoints.Count; i++)
         {
             var point = nodePoints[i];
             RaycastHit hit;
 
-            if(Physics.Raycast(new Vector3(point.x,100, point.y), Vector3.down, out hit))
+            if (Physics.Raycast(new Vector3(point.x, 100, point.y), Vector3.down, out hit))
             {
                 GameObject nodeGO = Instantiate(NodePointPrefab, transform);
                 nodeGO.name = "NodePoint " + i;
                 nodeGO.transform.position = hit.point;
-                Nodes[i] = nodeGO;
+                Nodes.Add(nodeGO);
             }
         }
+
+        //Obtenemos una malla con la triangulacion de Delaunay de Habrador
+        GenerateDelaunayTriangulation(nodePoints);
+
+        //TODO: Recorrer toda la malla, identificar todos los vertices con nodepoints y obtener las relaciones entre puntos con las aristas
+        //TODO: Eliminar edges demasiado largos
+    }
+
+    public void GenerateDelaunayTriangulation(List<Vector2> points)
+    {
+        DebugTrianglesMesh.sharedMesh = DelaunayTriangulation.GetDelaunayTriangleMesh(points);
     }
 
     private void EraseAllNodePoints()
